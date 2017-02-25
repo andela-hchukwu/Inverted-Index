38dfa9a7aac7550a9357d32335617b4c7d7d5fb3
andela-hchukwu <henry.chukwu@andela.com>
2017-02-25 04:01:50 +0100

    chore (test-suite): make a dummy test for coveralls [Fix #139456321]

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'src/inverted-index.js',
       'spec/inverted-index-test.js'
     ],
 
