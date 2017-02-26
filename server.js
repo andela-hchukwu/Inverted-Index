21ca29ec9cad4e961bae3ed2ed3e3a046d4d0d90
andela-hchukwu <henry.chukwu@andela.com>
2017-02-26 16:32:03 +0100

    chore (restructure-filefolder): restructure file folders of application [Fix #139456624]

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, '/src')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 const port = process.env.PORT || 9000;
 
 app.listen(port);
-console.log('server started on port' + port );
+console.log('server started on port' + port);
