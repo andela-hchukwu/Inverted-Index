f2c5e8de7e2b17613c2b715d8a3eb88964c1ea8c
andela-hchukwu <henry.chukwu@andela.com>
2017-02-27 16:52:54 +0100

    chore (travis-test): make test to pass travis [Fix #139456431]

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function(config) {
     basePath: '',
 
     plugins: [
-      'karma-firefox-launcher',
       'karma-chrome-launcher',
       'karma-jasmine'
     ],
@@ -62,7 +61,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
