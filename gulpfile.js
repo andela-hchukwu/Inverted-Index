be54c4b2619c111e14e1b6b537f6924a9ea9ef36
henry <henry.chukwu@andela.com>
2017-03-08 11:23:36 +0100

    chore(test-suite): modify main.css file for frontend [Fix #13965538]

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,14 @@ gulp.task('serve', () => {
   const browser1 = browserSync.create();
   browser1.init({
     server: {
-      baseDir: './'
+      baseDir: './src'
     },
     port: 5050,
     ui: {
       port: 9000
     }
   });
-  gulp.watch(['*src/html', 'src/js/**/*.js', 'src/css/*.css'])
+  gulp.watch(['*src/html', 'src/js/**/**/*.js', 'src/css/*.css'])
   .on('change', browser1.reload);
 });
 
-gulp.task('serveprod', () => {
-  connect.server({
-    root: './',
-    port: process.env.PORT || 5000 // localhost:5000
-  });
-});
